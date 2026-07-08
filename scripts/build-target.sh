adf8d1ec1913ebd28cd44a47e4f8fe62dc52a485
Yu <echo.aimless@gmail.com>
2026-07-08 14:14:39 +0800

    chore: rename folder + update refs

--- scripts/build-target.sh
+++ scripts/build-target.sh
@@ -66,7 +66,7 @@ if [ -z "$LIBRARY_PATH" ] || [ ! -f "$LIBRARY_PATH" ]; then
 fi
 
 cp -R "$SOURCE_DIR/include/ghostty" "$OUTPUT_DIR/include/"
-cp "scripts/templates/GhosttyVtPrebuilt.modulemap" "$OUTPUT_DIR/include/module.modulemap"
+cp "Scripts/templates/GhosttyVtPrebuilt.modulemap" "$OUTPUT_DIR/include/module.modulemap"
 
 cp "$LIBRARY_PATH" "$OUTPUT_DIR/lib/libghostty-vt.a"
 
